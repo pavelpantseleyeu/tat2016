a97026c988ab7ebbb57ff2e1b911245b47628b4b
pavelpantseleyeu <pavel_pantseleyeu@epam.com>
2016-10-16 17:20:55 +0300

    1

--- common.groovy
+++ common.groovy
@@ -1,9 +1,10 @@
 
 def getProps(filePath){
+    def input = new FileInputStream(new File(filePath));
     def yaml = new org.yaml.snakeyaml.Yaml()
-    def propsFile = readFile filePath
-    println propsFile
-    return yaml.load(propsFile)
+//    def propsFile = readFile filePath
+//    println propsFile
+    return yaml.load(input)
 }
 
 return this
