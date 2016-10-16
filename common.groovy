c7670f56cac1543544bfb629cff31c7457ed2e4c
pavelpantseleyeu <pavel_pantseleyeu@epam.com>
2016-10-16 19:46:45 +0300

    1

--- common.groovy
+++ common.groovy
@@ -1,8 +1,14 @@
 
 def getProps(filePath){
-    def input = new FileReader(filePath);
+    def input = new FileReader(filePath)
     def yaml = new org.yaml.snakeyaml.Yaml()
     return yaml.load(input)
 }
+
+def getPropJson(filePath){
+    def inputFile = new File(filePath)
+    def InputJSON = new JsonSlurper().parseText(inputFile)
+    return InputJSON
+    }
 
 return this
