5fa0fe182939e1f2fa0843749313f4bbffedcf28
pavelpantseleyeu <pavel_pantseleyeu@epam.com>
2016-10-16 17:46:10 +0300

    1

--- common.groovy
+++ common.groovy
@@ -1,6 +1,6 @@
 
 def getProps(filePath){
-    def input = new FileInputStream(new File(filePath));
+    def input = new FileInputStream(new File(filePath))
     def yaml = new org.yaml.snakeyaml.Yaml()
     return yaml.load(input)
 }
