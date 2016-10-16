0257056ac68bf3fb30a0fd3bcbc0bf021e10692d
pavelpantseleyeu <pavel_pantseleyeu@epam.com>
2016-10-16 16:58:19 +0300

    1

--- common.groovy
+++ common.groovy
@@ -1,4 +1,5 @@
 def getProps(filePath){
+    @Grab(group='org.yaml', module='snakeyaml', version='1.13')
     def yaml = new org.yaml.snakeyaml.Yaml()
     def propsFile = readFile filePath
     println propsFile
