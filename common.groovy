b349151878ef072fc342771129dcaa200672bcce
Pavel Pantseleyeu <Pavel_Pantseleyeu@epam.com>
2016-10-16 11:38:43 +0300

    Add files via upload

---
+++ common.groovy
@@ -0,0 +1,14 @@
+def _getProperties(filePath) {
+    def propsFile = readFile filePath
+    propsFile = propsFile.replace(':', '=').replace('  ','').replace('    ','').replace('-','').replace(' ','')
+    def props = new Properties()
+    props.load(new StringReader(propsFile))
+    return props
+}
+
+def readProperties(filePath){
+    def props = readProperties filePath
+    println props
+}
+
+return this
