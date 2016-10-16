9624950a655e829e0ae406f9b4a43d957df14027
pavelpantseleyeu <pavel_pantseleyeu@epam.com>
2016-10-16 14:32:22 +0300

    1

--- common.groovy
+++ common.groovy
@@ -15,8 +15,8 @@ def readProp2(path){
 def readProps(filePath){
     def yaml = new org.yaml.snakeyaml.Yaml()
     def propsFile = readFile filePath
-    def pr = yaml.load(propsFile)
-    return yaml.dump(pr, default_flow_style=False)
+    return yaml.load(propsFile)
+
 }
 
 @NonCPS
