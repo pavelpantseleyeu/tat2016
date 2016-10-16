22c784ec131c0ae7d95723b978da5d74bec46c7f
pavelpantseleyeu <pavel_pantseleyeu@epam.com>
2016-10-16 23:04:19 +0300

    1

--- common.groovy
+++ common.groovy
@@ -1,6 +1,6 @@
 
 def getJson(filePath){
-    def input = new FileReader(filePath)
+    def input = readFile filePath
     def slurper = new groovy.json.JsonSlurper()
     def text = slurper.parseText(input)
     return text
