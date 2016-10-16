d98f5286c5f705bee5eaa82b0473caeb7bd697f1
pavelpantseleyeu <pavel_pantseleyeu@epam.com>
2016-10-16 14:25:46 +0300

    1

--- common.groovy
+++ common.groovy
@@ -11,21 +11,12 @@ def readProp2(path){
     println item
 }
 
-//@Grab(group='org.yaml', module='snakeyaml', version='1.13')
+
 def readProps(filePath){
-
     def yaml = new org.yaml.snakeyaml.Yaml()
-    yaml.load(propsFile)
     def propsFile = readFile filePath
-    print propsFile
-    des pr = yaml.load(propsFile)
+    def pr = yaml.load(propsFile)
     print yaml.dump(pr)
-
-
-
-//    def props = new Properties()
-//    props.load(propsFile)
-//    return props
 }
 
 @NonCPS
