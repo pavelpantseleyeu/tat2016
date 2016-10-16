8558ff09ec8e9e171196a348c759371d5325f457
pavelpantseleyeu <pavel_pantseleyeu@epam.com>
2016-10-16 19:27:03 +0300

    1

--- common.groovy
+++ common.groovy
@@ -1,13 +1,6 @@
-def _getProperties(filePath) {
-    def propsFile = readFile filePath
-    propsFile = propsFile.replace(':', '=').replace('  ','').replace('    ','').replace('-','').replace(' ','')
-    def props = new Properties()
-    props.load(new StringReader(propsFile))
-    return props
-}
 
 def getProps(filePath){
-    def input = readFile filePath
+    def input = new FileReader(filePath);
     def yaml = new org.yaml.snakeyaml.Yaml()
     return yaml.load(input)
 }
