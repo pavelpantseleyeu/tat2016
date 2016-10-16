c57f0966bb41136ecf6301e4f63448fae549f24f
pavelpantseleyeu <pavel_pantseleyeu@epam.com>
2016-10-16 18:46:31 +0300

    1

--- common.groovy
+++ common.groovy
@@ -1,3 +1,10 @@
+def _getProperties(filePath) {
+    def propsFile = readFile filePath
+    propsFile = propsFile.replace(':', '=').replace('  ','').replace('    ','').replace('-','').replace(' ','')
+    def props = new Properties()
+    props.load(new StringReader(propsFile))
+    return props
+}
 
 def getProps(filePath){
     def input = readFile filePath
